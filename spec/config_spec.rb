db2b820ea1fc7d409f422e70f7bb30b246c0779c
Albert Wang <albert@cellabus.com>
2015-09-30 15:10:28 -0700

    Add tests for LaunchDarkly::Config

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,4 +2,39 @@ require 'spec_helper'
 
 describe LaunchDarkly::Config do
   subject { LaunchDarkly::Config }
+  describe '.initialize' do
+    it 'can be initialized with default settings' do
+      expect(subject).to receive(:default_capacity).and_return 1234
+      expect(subject.new.capacity).to eq 1234
+    end
+    it 'accepts custom arguments' do
+      expect(subject).to_not receive(:default_capacity)
+      expect(subject.new(capacity: 50).capacity).to eq 50
+    end
+    it 'will chomp base_url and stream_uri' do
+      uri = 'https://test.launchdarkly.com'
+      config = subject.new(base_uri: uri+'/')
+      expect(config.base_uri).to eq uri
+    end
+  end
+  describe '.default_store' do
+    it 'uses Rails cache if it is available' do
+      rails = instance_double('Rails', cache: :cache)
+      stub_const('Rails', rails)
+      expect(subject.default_store).to eq :cache
+    end
+    it 'uses memory store if Rails is not available' do
+      expect(subject.default_store).to be_an_instance_of LaunchDarkly::ThreadSafeMemoryStore
+    end
+  end
+  describe '.default_logger' do
+    it 'uses Rails logger if it is available' do
+      rails = instance_double('Rails', logger: :logger)
+      stub_const('Rails', rails)
+      expect(subject.default_logger).to eq :logger
+    end
+    it 'Uses logger if Rails is not available' do
+      expect(subject.default_logger).to be_an_instance_of Logger
+    end
+  end
 end
