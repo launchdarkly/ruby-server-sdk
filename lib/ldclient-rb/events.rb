f400b8fae57a976ed59daa3a0c690022e3abab1f
John Kodumal <john@launchdarkly.com>
2016-07-08 12:24:04 -0700

    Don't use http persistent for events

--- lib/ldclient-rb/events.rb
+++ lib/ldclient-rb/events.rb
@@ -10,7 +10,6 @@ module LaunchDarkly
       @config = config
       @client = Faraday.new do |builder|
         builder.use :http_cache, store: @config.cache_store
-        builder.adapter :net_http_persistent
       end
 
       @worker = create_worker
