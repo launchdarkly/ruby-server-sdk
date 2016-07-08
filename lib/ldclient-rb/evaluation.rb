b41c9121f7b405b440dccceb9597ac325f8d0afc
John Kodumal <john@launchdarkly.com>
2016-07-08 08:46:45 -0700

    Inline operators

--- lib/ldclient-rb/evaluation.rb
+++ lib/ldclient-rb/evaluation.rb
@@ -1,7 +1,6 @@
 module LaunchDarkly
 
   module Evaluation
-    include Operators
     BUILTINS = [:key, :ip, :country, :email, :firstName, :lastName, :avatar, :name, :anonymous]
 
     class EvaluationError < StandardError
@@ -133,7 +132,7 @@ module LaunchDarkly
 
       @config.logger.debug("Got user value #{val} for #{clause[:attribute]} and user #{user}")
 
-      op = Operators::operators[clause[:op]]
+      op = operators[clause[:op]]
 
       if val.is_a? Enumerable
         @config.logger.debug("User value #{val} is enumerable")
@@ -203,6 +202,58 @@ module LaunchDarkly
       end
       return false
     end
+
+    operators = {
+      in: 
+        lambda do |a, b|
+          a == b
+        end,
+      endsWith: 
+        lambda do |a, b|
+          (a.is_a? String) && (a.end_with? b)
+        end,
+      startsWith: 
+        lambda do |a, b|
+          (a.is_a? String) && (a.start_with? b)
+        end,
+      matches:
+        lambda do |a, b|
+          (a.is_a? String) && !(Regexp.new a).match(b).nil?
+        end,
+      contains:
+        lambda do |a, b|
+          (a.is_a? String) && (a.include? b)
+        end,
+      lessThan:
+        lambda do |a, b|
+          (a.is_a? Numeric) && (a < b)
+        end,
+      lessThanOrEqual:
+        lambda do |a, b|
+          (a.is_a? Numeric) && (a <= b)
+        end,
+      greaterThan:
+        lambda do |a, b|
+          (a.is_a? Numeric) && (a > b)
+        end,
+      greaterThanOrEqual:
+        lambda do |a, b|
+          (a.is_a? Numeric) && (a >= b)
+        end,
+      greaterThanOrEqual:
+        lambda do |a, b|
+          (a.is_a? Numeric) && (a >= b)
+        end,
+      before:
+        lambda do |a, b|
+          ((a.is_a? Numeric) && (a < b)) || ((a.is_a? String) && (DateTime.rfc3339(a).to_time.utc < DateTime.rfc3339(b).to_time.utc))
+        end,
+      after:
+        lambda do |a, b|
+          ((a.is_a? Numeric) && (a > b)) || ((a.is_a? String) && (DateTime.rfc3339(a).to_time.utc > DateTime.rfc3339(b).to_time.utc))
+        end
+    }
+
   end
 
 end
--- lib/ldclient-rb/operators.rb
+++ lib/ldclient-rb/operators.rb
@@ -2,55 +2,5 @@ require "date"
 
 module LaunchDarkly
   module Operators
-    operators = {
-      in: 
-        lambda do |a, b|
-          a == b
-        end,
-      endsWith: 
-        lambda do |a, b|
-          (a.is_a? String) && (a.end_with? b)
-        end,
-      startsWith: 
-        lambda do |a, b|
-          (a.is_a? String) && (a.start_with? b)
-        end,
-      matches:
-        lambda do |a, b|
-          (a.is_a? String) && !(Regexp.new a).match(b).nil?
-        end,
-      contains:
-        lambda do |a, b|
-          (a.is_a? String) && (a.include? b)
-        end,
-      lessThan:
-        lambda do |a, b|
-          (a.is_a? Numeric) && (a < b)
-        end,
-      lessThanOrEqual:
-        lambda do |a, b|
-          (a.is_a? Numeric) && (a <= b)
-        end,
-      greaterThan:
-        lambda do |a, b|
-          (a.is_a? Numeric) && (a > b)
-        end,
-      greaterThanOrEqual:
-        lambda do |a, b|
-          (a.is_a? Numeric) && (a >= b)
-        end,
-      greaterThanOrEqual:
-        lambda do |a, b|
-          (a.is_a? Numeric) && (a >= b)
-        end,
-      before:
-        lambda do |a, b|
-          ((a.is_a? Numeric) && (a < b)) || ((a.is_a? String) && (DateTime.rfc3339(a).to_time.utc < DateTime.rfc3339(b).to_time.utc))
-        end,
-      after:
-        lambda do |a, b|
-          ((a.is_a? Numeric) && (a > b)) || ((a.is_a? String) && (DateTime.rfc3339(a).to_time.utc > DateTime.rfc3339(b).to_time.utc))
-        end
-    }
   end
 end
