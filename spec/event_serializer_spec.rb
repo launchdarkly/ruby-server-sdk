f164748ca38560c317bae4493afb61b29cb6abf9
Eli Bishop <this@errorbar.net>
2018-01-12 12:41:19 -0800

    strip unknown top-level user attrs; never make "anonymous" attribute private (#29)

--- spec/event_serializer_spec.rb
+++ spec/event_serializer_spec.rb
@@ -19,6 +19,14 @@ describe LaunchDarkly::EventSerializer do
     u
   }
 
+  let(:user_with_unknown_top_level_attrs) {
+    { key: 'abc', firstName: 'Sue', species: 'human', hatSize: 6, custom: { bizzle: 'def', dizzle: 'ghi' }}
+  }
+
+  let(:anon_user) {
+    { key: 'abc', anonymous: 'true', custom: { bizzle: 'def', dizzle: 'ghi' }}
+  }
+
   # expected results from serializing user
 
   let(:user_with_all_attrs_hidden) {
@@ -31,6 +39,10 @@ describe LaunchDarkly::EventSerializer do
 
   let(:user_with_own_specified_attr_hidden) {
     { key: 'abc', firstName: 'Sue', custom: { bizzle: 'def' }, privateAttrs: [ 'dizzle' ]}
+  }
+
+  let(:anon_user_with_all_attrs_hidden) {
+    { key: 'abc', anonymous: 'true', custom: { }, privateAttrs: [ 'bizzle', 'dizzle' ]}
   }
 
 
@@ -81,6 +93,20 @@ describe LaunchDarkly::EventSerializer do
       event = make_event(user_specifying_own_private_attr)
       j = es.serialize_events([event])
       expect(parse_results(j)).to eq [make_event(user_with_all_attrs_hidden)]
+    end
+
+    it "strips out any unknown top-level attributes" do
+      es = LaunchDarkly::EventSerializer.new(base_config)
+      event = make_event(user_with_unknown_top_level_attrs)
+      j = es.serialize_events([event])
+      expect(parse_results(j)).to eq [make_event(user)]
+    end
+
+    it "leaves the anonymous attribute as is" do
+      es = LaunchDarkly::EventSerializer.new(config_with_all_attrs_private)
+      event = make_event(anon_user)
+      j = es.serialize_events([event])
+      expect(parse_results(j)).to eq [make_event(anon_user_with_all_attrs_hidden)]
     end
   end
 end
