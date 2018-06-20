bffde7fe0897241d7b5f17fbc30eec60319053f3
Eli Bishop <eli@launchdarkly.com>
2018-06-20 14:21:36 -0700

    refactor for better encapsulation of HTTP logic

--- lib/ldclient-rb/sse_client.rb
+++ lib/ldclient-rb/sse_client.rb
@@ -14,7 +14,6 @@ module LaunchDarkly
   class SSEClient
     DEFAULT_CONNECT_TIMEOUT = 10
     DEFAULT_READ_TIMEOUT = 300
-    DEFAULT_CHUNK_SIZE = 10000
     DEFAULT_RECONNECT_TIME = 1
     MAX_RECONNECT_TIME = 30
 
@@ -25,7 +24,6 @@ module LaunchDarkly
       @headers = options[:headers].clone || {}
       @connect_timeout = options[:connect_timeout] || DEFAULT_CONNECT_TIMEOUT
       @read_timeout = options[:read_timeout] || DEFAULT_READ_TIMEOUT
-      @chunk_size = options[:chunk_size] || DEFAULT_CHUNK_SIZE
       @logger = options[:logger] || default_logger
 
       proxy = ENV['HTTP_PROXY'] || ENV['http_proxy'] || options[:proxy]
@@ -77,13 +75,15 @@ module LaunchDarkly
 
     def run_stream
       while !@stopped.value
+        cxn = nil
         begin
-          connect
-          read_stream
+          cxn = connect
+          read_stream(cxn)
         rescue ShutdownSignal
           return
         rescue StandardError => e
-          @logger.error("Unexpected error from event source: #{e.inspect}")
+          @logger.error("Unexpected error from event source: #{e.inspect} #{e.backtrace}")
+          cxn.close if !cxn.nil?
         end
       end
     end
@@ -96,42 +96,24 @@ module LaunchDarkly
           sleep(interval)
         end
         begin
-          if @proxy
-            @socket = open_socket(@proxy)
-            @socket.write(build_proxy_request)
-          else
-            @socket = open_socket(@uri)
+          cxn = StreamingHTTPConnection.new(@uri, @proxy, build_headers, @connect_timeout, @read_timeout)
+          resp_status, resp_headers = cxn.read_headers
+          if resp_status != 200
+            body = cxn.consume_body
+            cxn.close
+            @on[:error].call({status_code: resp_status, body: body})
+          elsif resp_headers["content-type"] && resp_headers["content-type"].start_with?("text/event-stream")
+            return cxn
           end
-          @socket.write(build_request)
-          
-          @parser = ResponseParser.new
-          while !@parser.headers? && read_chunk(@parser)
-          end
-
-          if @parser.status != 200
-            # Consume response body if present
-            while !@parser.done? && read_chunk(@parser)
-            end
-            close
-            @on[:error].call({status_code: @parser.status, body: @parser.buffer})
-            return false
-          end
-          if @parser.headers["content-type"] && @parser.headers["content-type"].start_with?("text/event-stream")
-            return
-          end
-          @logger.error("Event source returned unexpected content type '#{@parser.headers["content-type"]}'")
+          @logger.error("Event source returned unexpected content type '#{resp_headers["content-type"]}'")
         rescue StandardError => e
-          @logger.error("Unexpected error from event source: #{e.inspect}")
+          @logger.error("Unexpected error from event source: #{e.inspect} #{e.backtrace}")
+          cxn.close if !cxn.nil?
         end
       end
     end
 
-    def disconnect
-      @socket.close if @socket
-      @socket = nil
-    end
-
-    def read_stream
+    def read_stream(cxn)
       event_parser = EventParser.new
       event_parser.on(:event) do |event|
         dispatch_event(event)
@@ -139,13 +121,8 @@ module LaunchDarkly
       event_parser.on(:retry) do |interval|
         @backoff.base_interval = interval / 1000
       end
-      loop do
-        line = @parser.readline
-        if line.nil?
-          return if !read_chunk(@parser)
-        else
-          event_parser << line
-        end
+      cxn.read_lines.each do |line|
+        event_parser << line
       end
     end
 
@@ -155,99 +132,127 @@ module LaunchDarkly
       @on[:event].call(event)
     end
 
-    def open_socket(uri)
-      if uri.scheme == 'https'
-        Socketry::SSL::Socket.connect(uri.host, uri.port, timeout: @connect_timeout)
-      else
-        Socketry::TCP::Socket.connect(uri.host, uri.port, timeout: @connect_timeout)
-      end
-    end
-
-    def build_request
-      buf = "GET #{@uri.request_uri} HTTP/1.1\r\n"
-      all_headers = @headers.merge(base_headers)
-      all_headers.each { |k, v|
-        buf << "#{k}: #{v}\r\n"
-      }
-      buf + "\r\n"
-    end
-
-    def base_headers
+    def build_headers
       h = {
         'Accept' => 'text/event-stream',
         'Cache-Control' => 'no-cache',
         'Host' => @uri.host
       }
       h['Last-Event-Id'] = @last_id if !@last_id.nil?
-      h
-    end
-
-    def build_proxy_request
-      buf = "CONNECT #{@uri.host}:#{@uri.port} HTTP/1.1\r\n"
-      buf << "Host: #{@uri.host}:#{@uri.port}\r\n"
-      if @proxy.user || @proxy.password
-        encoded_credentials = Base64.strict_encode64([@proxy.user || '', @proxy.password || ''].join(":"))
-        buf << "Proxy-Authorization: Basic #{encoded_credentials}\r\n"
-      end
-      buf << "\r\n"
-      buf
-    end
-
-    def read_chunk(sink)
-      data = @socket.readpartial(@chunk_size, timeout: @read_timeout)
-      return false if data == :eof
-      sink << data
-      true
+      h.merge(@headers)
     end
   end
 
   class ShutdownSignal < StandardError
   end
 
-  class ResponseParser
-    def initialize
+  class StreamingHTTPConnection
+    DEFAULT_CHUNK_SIZE = 10000
+
+    def initialize(uri, proxy, headers, connect_timeout, read_timeout)
       @parser = HTTPTools::Parser.new
-      @done = false
+      @headers = nil
       @buffer = ""
+      @read_timeout = read_timeout
+      @done = false
       @lock = Mutex.new
+
       @parser.on(:header) do
         @headers = Hash[@parser.header.map { |k,v| [k.downcase, v] }]
       end
       @parser.on(:stream) do |data|
-        @lock.synchronize do
-          @buffer << data
-        end
+        @lock.synchronize { @buffer << data }
       end
       @parser.on(:finish) do
-        @done = true
+        @lock.synchronize { @done = true }
+      end
+
+      if proxy
+        @socket = open_socket(proxy, connect_timeout)
+        @socket.write(build_proxy_request(uri, proxy))
+      else
+        @socket = open_socket(uri, connect_timeout)
+      end
+
+      @socket.write(build_request(uri, headers))
+    end
+
+    def close
+      @socket.close if @socket
+      @socket = nil
+    end
+
+    def read_headers
+      while @headers.nil? && read_chunk
+      end
+      [@parser.status_code, @headers]
+    end
+
+    def read_lines
+      Enumerator.new do |gen|
+        loop do
+          line = read_line
+          break if line.nil?
+          gen.yield line
+        end
       end
     end
 
-    attr_reader :headers
-    attr_reader :buffer
+    def consume_body
+      loop do
+        @lock.synchronize { break if @done }
+        break if !read_chunk
+      end
+      @buffer
+    end
 
-    def <<(data)
+    private
+
+    def open_socket(uri, connect_timeout)
+      if uri.scheme == 'https'
+        Socketry::SSL::Socket.connect(uri.host, uri.port, timeout: connect_timeout)
+      else
+        Socketry::TCP::Socket.connect(uri.host, uri.port, timeout: connect_timeout)
+      end
+    end
+
+    def build_request(uri, headers)
+      ret = "GET #{uri.request_uri} HTTP/1.1\r\n"
+      headers.each { |k, v|
+        ret << "#{k}: #{v}\r\n"
+      }
+      ret + "\r\n"
+    end
+
+    def build_proxy_request(uri, proxy)
+      ret = "CONNECT #{uri.host}:#{uri.port} HTTP/1.1\r\n"
+      ret << "Host: #{uri.host}:#{uri.port}\r\n"
+      if proxy.user || proxy.password
+        encoded_credentials = Base64.strict_encode64([proxy.user || '', proxy.password || ''].join(":"))
+        ret << "Proxy-Authorization: Basic #{encoded_credentials}\r\n"
+      end
+      ret << "\r\n"
+      ret
+    end
+
+    def read_chunk
+      data = @socket.readpartial(DEFAULT_CHUNK_SIZE, timeout: @read_timeout)
+      return false if data == :eof
       @parser << data
+      true
     end
 
-    def done?
-      @done
-    end
-
-    def headers?
-      !!@headers
-    end
-
-    def status
-      @parser.status_code
-    end
-
-    def readline()
-      @lock.synchronize do
-        i = @buffer.index(/[\r\n]/)
-        return nil if i.nil?
-        i += 1 if (@buffer[i] == "\r" && i < @buffer.length - 1 && @buffer[i + 1] == "\n")
-        @buffer.slice!(0, i + 1).force_encoding(Encoding::UTF_8)
+    def read_line
+      loop do
+        @lock.synchronize do
+          return nil if @done
+          i = @buffer.index(/[\r\n]/)
+          if !i.nil?
+            i += 1 if (@buffer[i] == "\r" && i < @buffer.length - 1 && @buffer[i + 1] == "\n")
+            return @buffer.slice!(0, i + 1).force_encoding(Encoding::UTF_8)
+          end
+        end
+        return nil if !read_chunk
       end
     end
   end
