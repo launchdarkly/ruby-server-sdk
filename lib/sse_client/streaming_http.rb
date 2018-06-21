d0b257b29f3471a235ef809fd6f10420560946ce
Eli Bishop <eli@launchdarkly.com>
2018-06-21 15:43:04 -0700

    break out HTTP response reader into its own class

--- lib/sse_client/streaming_http.rb
+++ lib/sse_client/streaming_http.rb
@@ -3,40 +3,11 @@ require "socketry"
 
 module SSE
   #
-  # Wrapper around a socket allowing us to read an HTTP response incrementally, line by line,
-  # or to consume the entire response body.
-  #
-  # The socket is managed by Socketry, which implements the read timeout.
-  #
-  # Incoming data is fed into an instance of HTTPTools::Parser, which gives us the header and
-  # chunks of the body via callbacks.
+  # Wrapper around a socket providing a simplified HTTP request-response cycle including streaming.
+  # The socket is created and managed by Socketry, which we use so that we can have a read timeout.
   #
   class StreamingHTTPConnection
-    DEFAULT_CHUNK_SIZE = 10000
-
-    attr_reader :status
-    attr_reader :headers
-
     def initialize(uri, proxy, headers, connect_timeout, read_timeout)
-      @parser = HTTPTools::Parser.new
-      @buffer = ""
-      @read_timeout = read_timeout
-      @done = false
-      @lock = Mutex.new
-
-      # Provide callbacks for the Parser to give us the headers and body. This has to be done
-      # before we start piping any data into the parser.
-      have_headers = false
-      @parser.on(:header) do
-        have_headers = true
-      end
-      @parser.on(:stream) do |data|
-        @lock.synchronize { @buffer << data }
-      end
-      @parser.on(:finish) do
-        @lock.synchronize { @done = true }
-      end
-
       if proxy
         @socket = open_socket(proxy, connect_timeout)
         @socket.write(build_proxy_request(uri, proxy))
@@ -46,12 +17,7 @@ module SSE
 
       @socket.write(build_request(uri, headers))
 
-      # Block until the status code and headers have been successfully read.
-      while !have_headers
-        raise EOFError if !read_chunk_into_buffer
-      end
-      @headers = Hash[@parser.header.map { |k,v| [k.downcase, v] }]
-      @status = @parser.status_code
+      @reader = HTTPResponseReader.new(@socket, read_timeout)
     end
 
     def close
@@ -59,25 +25,23 @@ module SSE
       @socket = nil
     end
 
+    def status
+      @reader.status
+    end
+
+    def headers
+      @reader.headers
+    end
+
     # Generator that returns one line of the response body at a time (delimited by \r, \n,
     # or \r\n) until the response is fully consumed or the socket is closed.
     def read_lines
-      Enumerator.new do |gen|
-        loop do
-          line = read_line
-          break if line.nil?
-          gen.yield line
-        end
-      end
+      @reader.read_lines
     end
 
     # Consumes the entire response body and returns it.
-    def consume_body
-      loop do
-        @lock.synchronize { break if @done }
-        break if !read_chunk_into_buffer
-      end
-      @buffer
+    def read_all
+      @reader.read_all
     end
 
     private
@@ -110,9 +74,63 @@ module SSE
       ret << "\r\n"
       ret
     end
+  end
+
+  #
+  # Used internally to read the HTTP response, either all at once or as a stream of text lines.
+  # Incoming data is fed into an instance of HTTPTools::Parser, which gives us the header and
+  # chunks of the body via callbacks.
+  #
+  class HTTPResponseReader
+    DEFAULT_CHUNK_SIZE = 10000
+
+    attr_reader :status, :headers
+
+    def initialize(socket, read_timeout)
+      @socket = socket
+      @read_timeout = read_timeout
+      @parser = HTTPTools::Parser.new
+      @buffer = ""
+      @lock = Mutex.new
+
+      # Provide callbacks for the Parser to give us the headers and body. This has to be done
+      # before we start piping any data into the parser.
+      have_headers = false
+      @parser.on(:header) do
+        have_headers = true
+      end
+      @parser.on(:stream) do |data|
+        @lock.synchronize { @buffer << data }  # synchronize because we're called from another thread in Socketry
+      end
+
+      # Block until the status code and headers have been successfully read.
+      while !have_headers
+        raise EOFError if !read_chunk_into_buffer
+      end
+      @headers = Hash[@parser.header.map { |k,v| [k.downcase, v] }]
+      @status = @parser.status_code
+    end
+
+    def read_lines
+      Enumerator.new do |gen|
+        loop do
+          line = read_line
+          break if line.nil?
+          gen.yield line
+        end
+      end
+    end
+
+    def read_all
+      while read_chunk_into_buffer
+      end
+      @buffer
+    end
+
+    private
 
     # Attempt to read some more data from the socket. Return true if successful, false if EOF.
-    # A read timeout will result in an exception.
+    # A read timeout will result in an exception from Socketry's readpartial method.
     def read_chunk_into_buffer
       data = @socket.readpartial(DEFAULT_CHUNK_SIZE, timeout: @read_timeout)
       return false if data == :eof
@@ -126,7 +144,6 @@ module SSE
     def read_line
       loop do
         @lock.synchronize do
-          return nil if @done
           i = @buffer.index(/[\r\n]/)
           if !i.nil?
             i += 1 if (@buffer[i] == "\r" && i < @buffer.length - 1 && @buffer[i + 1] == "\n")
