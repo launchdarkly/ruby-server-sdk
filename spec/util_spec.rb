d4be186ed2026056dd9768fd2b265181f9353c72
Eli Bishop <eli@launchdarkly.com>
2018-08-01 17:48:15 -0700

    re-add minimal unit test

---
+++ spec/util_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe LaunchDarkly::Util do
+  describe 'log_exception' do
+    let(:logger) { double() }
+
+    it "logs error data" do
+      expect(logger).to receive(:warn)
+      expect(logger).to receive(:debug)
+      begin
+        raise StandardError.new 'asdf'
+      rescue StandardError => exn
+        LaunchDarkly::Util.log_exception(logger, "message", exn)
+      end
+    end
+  end
+end
