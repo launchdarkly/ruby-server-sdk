ef834c7387d82fca92375ee7e37b85a5a95930e3
Eli Bishop <eli@launchdarkly.com>
2018-10-24 09:45:33 -0700

    prepare 5.3.0 release (#108)

--- lib/ldclient-rb/flags_state.rb
+++ lib/ldclient-rb/flags_state.rb
@@ -15,13 +15,21 @@ module LaunchDarkly
     end
 
     # Used internally to build the state map.
-    def add_flag(flag, value, variation, reason = nil)
+    def add_flag(flag, value, variation, reason = nil, details_only_if_tracked = false)
       key = flag[:key]
       @flag_values[key] = value
-      meta = { version: flag[:version], trackEvents: flag[:trackEvents] }
+      meta = {}
+      with_details = !details_only_if_tracked || flag[:trackEvents]
+      if !with_details && flag[:debugEventsUntilDate]
+        with_details = flag[:debugEventsUntilDate] > (Time.now.to_f * 1000).to_i
+      end
+      if with_details
+        meta[:version] = flag[:version]
+        meta[:reason] = reason if !reason.nil?
+      end
       meta[:variation] = variation if !variation.nil?
+      meta[:trackEvents] = true if flag[:trackEvents]
       meta[:debugEventsUntilDate] = flag[:debugEventsUntilDate] if flag[:debugEventsUntilDate]
-      meta[:reason] = reason if !reason.nil?
       @flag_metadata[key] = meta
     end
 
