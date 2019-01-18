ebd0f97eb5461a610000c0ac853e038868cad05e
Eli Bishop <eli@launchdarkly.com>
2019-01-18 11:57:10 -0800

    prepare 5.5.2 release (#123)

--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -6,7 +6,6 @@
 gem install --conservative yard
 gem install --conservative redcarpet  # provides Markdown formatting
 
-# yard doesn't seem to do recursive directories, even though Ruby's Dir.glob supposedly recurses for "**"
-PATHS="lib/*.rb lib/**/*.rb lib/**/**/*.rb lib/**/**/**/*.rb"
+rm -rf doc/*
 
-yard doc --no-private --markup markdown --markup-provider redcarpet --embed-mixins $PATHS - README.md
+yard doc
