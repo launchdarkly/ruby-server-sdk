4ff45fce8502d49d8b3eb145d2012a69c535a29c
Eli Bishop <eli@launchdarkly.com>
2021-09-23 13:27:18 -0700

    use Releaser v2 config (#161)

--- .ldrelease/build-docs.sh
+++ .ldrelease/build-docs.sh
@@ -2,17 +2,14 @@
 
 # doc generation is not part of Releaser's standard Ruby project template
 
-mkdir -p ./artifacts/
-
 cd ./docs
 make
-cd ..
+cd build/html
 
-# Releaser will pick up docs generated in CI if we put an archive of them in the
-# artifacts directory and name it docs.tar.gz or docs.zip. They will be uploaded
-# to GitHub Pages and also attached as release artifacts. There's no separate
-# "publish-docs" step because the external service that also hosts them doesn't
-# require an upload, it just picks up gems automatically.
+# Releaser will pick up generated docs if we put them in the designated
+# directory. They will be uploaded to GitHub Pages and also attached as
+# release artifacts. There's no separate "publish-docs" step because the
+# external service that also hosts them doesn't require an upload, it just
+# picks up gems automatically.
 
-cd ./docs/build/html
-tar cfz ../../../artifacts/docs.tar.gz *
+cp -r * "${LD_RELEASE_DOCS_DIR}"
